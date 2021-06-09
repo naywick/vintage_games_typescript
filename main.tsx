dd93a5b1590fa16a58860dcb8bf7f939fea3aefd
Tom Morton <tom.m@38degrees.org.uk>
2021-06-09 11:53:58 +0100

    Updated dependencies in package.json. some work on checkboxyness

--- main.tsx
+++ main.tsx
@@ -7,13 +7,24 @@ ReactDOM.render(
 );
 
 type TaskType = { label: string; checked: boolean };
+
 function MyRootComponent(props: { children: string; myProperty: number }) {
   const [ counter1, setCounter1 ] = React.useState(0);
   const [ tasks, setTasks ] = React.useState<TaskType[]>([])
   const [ myInputValue, setMyInputValue ] = React.useState("");
+  //const [ checkboxValue, setCheckboxValue ] = React.useState()
+
+  function handleToggleTask(task: TaskType, checked: boolean) {
+    console.debug(`Task was toggled: ${JSON.stringify(task)}, ${checked}`);
+    const newTask = { ...task, checked: checked };
+    const newList = tasks.filter(t => t !== task);
+    newList.push(newTask);
+    setTasks(newList);
+  }
+
   const taskComponents = [];
   for (let task of tasks){
-    taskComponents.push(<Task task={task} />);
+    taskComponents.push(<Task task={task} onToggleChecked={handleToggleTask} />);
   }
 
   function addTask() {
@@ -27,17 +38,22 @@ return <>
   <ResetButton counter={counter1} setCounter={setCounter1}></ResetButton>
   <ButtonComponent counter={counter1} setCounter={setCounter1}></ButtonComponent>
   {taskComponents}
-  <DeleteCompletedTasksButton tasks={tasks}></DeleteCompletedTasksButton>
+  {/*<DeleteCompletedTasksButton tasks={tasks}></DeleteCompletedTasksButton>*/}
 
   <input type={'text'} value={myInputValue} onChange={(e) => setMyInputValue(e.target.value) }></input>
   <button onClick={addTask}>Submit</button>
   </>
 };
 
-function Task(props: { task: TaskType }) {
+type OnToggleCheckedHandler = (task: TaskType, checked: boolean) => void;
+
+function Task(props: { task: TaskType; onToggleChecked: OnToggleCheckedHandler }) {
   return <>
-  <input type={'checkbox'} onChange={ (e) => checkboxValue(e.target.value) }></input>
-  <p>{props.task.label}</p>
+  <input type={'checkbox'} onChange={ (e) => props.onToggleChecked(props.task, e.target.checked) }></input>
+  { props.task.checked
+    ? <p>{props.task.label}</p>
+    : <p><b>{props.task.label}</b></p>
+  }
   </>
 };
 
