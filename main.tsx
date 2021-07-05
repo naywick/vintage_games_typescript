4a6f0fcf0fe3c243f800f2b60edc23a6f5242dcd
Nayela Wickramasuriya <nayelawick@gmail.com>
2021-07-05 12:31:18 +0100

    Unknown changes WIP

--- main.tsx
+++ main.tsx
@@ -6,43 +6,57 @@ ReactDOM.render(
   document.body
 );
 
-type TaskType = { label: string; checked: boolean };
+type TaskType = { label: string; checked: boolean, id: number };
 
 function MyRootComponent(props: { children: string; myProperty: number }) {
   const [ tasks, setTasks ] = React.useState<TaskType[]>([])
   const [ myInputValue, setMyInputValue ] = React.useState("");
   //const [ checkboxValue, setCheckboxValue ] = React.useState()
 
-  function handleToggleTask(task_index: number, checked: boolean) {
+  function handleToggleTask(task: TaskType, checked: boolean) {
     const newList = [ ...tasks ];
-    const newTask = { ...tasks[task_index], checked: checked };
-    newList[task_index] = newTask;
+    const newTask = { ...tasks[tasks.indexOf(task)], checked: checked };
+    newList[tasks.indexOf(task)] = newTask;
     setTasks(newList);
-  }
+  };
 
   function addTask() {
-    let newTasks = [...tasks, {label: myInputValue, checked: false} ]
+    let newTasks = [...tasks, {label: myInputValue, checked: false, id: Math.random()} ]
     setTasks(newTasks)
     setMyInputValue("");
+  };
+
+  function deleteCheckedTasks() {
+    let remainingTasks = tasks.filter(task => {
+      return !task.checked 
+    })
+    setTasks(remainingTasks)
+  };
+
+  function handleEnterKey(e: any) {
+    if (e.key === 'Enter') {
+      addTask()
+    }
   };
 
 return <> 
   <h1>Blah { props.children } { props.myProperty }</h1>
   <div>
-    { tasks.map((t, idx) => <Task task={t} task_index={idx} onToggleChecked={handleToggleTask} />) }
+    { tasks.map((t, idx) => <Task task={t} task_index={idx} key={t.id} onToggleChecked={handleToggleTask} />) }
   </div>
   {/*<DeleteCompletedTasksButton tasks={tasks}></DeleteCompletedTasksButton>*/}
 
-  <input type={'text'} value={myInputValue} onChange={(e) => setMyInputValue(e.target.value) }></input>
+  <input type={'text'} value={myInputValue} onChange={(e) => setMyInputValue(e.target.value) } onKeyDown={handleEnterKey}></input>
   <button onClick={addTask}>Submit</button>
+  <button onClick={deleteCheckedTasks}>Delete Completed</button>
   </>
 };
 
-type OnToggleCheckedHandler = (task_index: number, checked: boolean) => void;
+type OnToggleCheckedHandler = (task: TaskType, checked: boolean) => void;
 
 function Task(props: { task: TaskType, task_index: number; onToggleChecked: OnToggleCheckedHandler }) {
   return <div>
-  <input type={'checkbox'} onChange={ (e) => props.onToggleChecked(props.task_index, e.target.checked) }></input>
+  <input type={'checkbox'} onChange={ (e) => props.onToggleChecked(props.task, e.target.checked) }></input>
   { props.task.checked &&
       <span style={{textDecoration: "line-through"}}>{props.task.label}</span>
   }
